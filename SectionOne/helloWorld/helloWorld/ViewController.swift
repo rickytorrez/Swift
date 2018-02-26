cddb76696b03a3babe2b01ce8dbe5be1ce3aa8e2
rickytorrez <torrez.ricky@ymail.com>
2018-02-26 14:25:50 -0500

    first commit

---
+++ SectionOne/helloWorld/helloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  helloWorld
+//
+//  Created by Ricky Torrez on 2/26/18.
+//  Copyright © 2018 E. Ricardo Torrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
