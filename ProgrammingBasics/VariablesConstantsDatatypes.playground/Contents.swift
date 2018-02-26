c8f1d21c71ee1e383507cd957348fea6b0abd835
rickytorrez <torrez.ricky@ymail.com>
2018-02-26 17:05:08 -0500

    Variables

---
+++ ProgrammingBasics/VariablesConstantsDatatypes.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 27
+
+myAge = 28
+
+let myName : String = "Ricardo"
+
+let myLastName : String = "Torrez"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName)  \(myLastName)"
+
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 12
+
+let text : String = "abc"
+
+let boolean : Bool = true
+
+let floaringPointNumber : Float = 1.3
+
+let double : Double = 3.14159265359
+
