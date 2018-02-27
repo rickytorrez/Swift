34300cf70e775e039f910f4d44fce519994ec353
rickytorrez <torrez.ricky@ymail.com>
2018-02-27 14:01:07 -0500

    Magic 8 Ball

---
+++ DataTypes-Variables-Arrays/magicEightBall/magicEightBall/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  magicEightBall
+//
+//  Created by Ricky Torrez on 2/27/18.
+//  Copyright © 2018 E. Ricardo Torrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    var randomBallNumber : Int = 0
+
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newBallImage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    func newBallImage(){
+        randomBallNumber = Int(arc4random_uniform(5))
+        ImageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+}
+
