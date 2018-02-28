a667bec7474ef2b427a11f43899302f64f9c97b7
rickytorrez <torrez.ricky@ymail.com>
2018-02-28 11:57:43 -0500

    Loops

---
+++ SwiftPlaygrounds/Loops.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let arrayOfNumbers = [1,5,4,2,6,8,9,6,4,2]
+
+var sum = 0
+
+for number in arrayOfNumbers {
+    sum += number
+}
+// print(sum)
+
+
+for number in 1...10 where number % 2 == 0{
+//    print(number)
+}
+
+
+
+
+
