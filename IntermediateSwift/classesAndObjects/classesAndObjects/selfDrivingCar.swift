4e1c50b119dd728d926ac1dccd2c3472002adba3
rickytorrez <torrez.ricky@ymail.com>
2018-03-04 13:21:22 -0500

    Class Inheritance

---
+++ IntermediateSwift/classesAndObjects/classesAndObjects/selfDrivingCar.swift
@@ -0,0 +1,15 @@
+//
+//  selfDrivingCar.swift
+//  classesAndObjects
+//
+//  Created by Ricky Torrez on 3/4/18.
+//  Copyright © 2018 E. Ricardo Torrez. All rights reserved.
+//
+
+import Foundation
+
+class SelfDrivingCar : Car {
+    
+    
+    
+}
